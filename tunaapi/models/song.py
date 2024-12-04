1ee514f6fa5bc0fedcc1ab539f3d823b26495447
Melissa Carter <mkd.princess@gmail.com>
2024-12-03 20:42:29 -0600

    added package

--- tunaapi/models/song.py
+++ tunaapi/models/song.py
@@ -1,5 +1,5 @@
 from django.db import models  # Import base class from Django stdlib
-from .artists import Artist
+from .artist import Artist
 
 class Song(models.Model):  # Must inherit from this base class
     title = models.CharField(max_length=75)  # Define all non-id fields
